f403908586c8380eba95c0241fc567f2d8f1f6af
etalay <talay.emre@gmail.com>
2015-10-22 20:33:47 +0300

    Thread Class implementation'u gerçekleştirdim.

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -6,6 +6,8 @@ Created on Thu Oct 22 15:55:37 2015
 """
 
 import string
+import threading
+import time
 
 
 #Kriptolama icin kullandigimiz algoritma.
@@ -19,3 +21,25 @@ def caesar(substring, shift_number, decode = False):
            )
        )
 
+#thread class'larinin olusturulmasi.
+class myThread (threading.Thread):
+    def __init__(self, threadID,counter,fread,fwrite,shiftNumber):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.counter = counter
+        self.fread = fread
+        self.fwrite = fwrite
+        self.shiftNumber = shiftNumber
+    def run(self):
+        global exit
+        while not exit:
+            threadLock.acquire()
+            buffin=fread.read(self.counter)
+            if not buffin:
+                exit = 1
+                threadLock.release()
+            else:
+                buffout=caesar(buffin,self.shiftNumber).upper()             
+                fwrite.write(''.join(buffout))
+                threadLock.release()
+            time.sleep(0.001)
