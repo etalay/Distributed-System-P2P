1ea47fac84c719c49f5161fde33dad87668e3050
etalay <talay.emre@gmail.com>
2015-11-17 19:24:45 +0200

    Mesaj alici thread İmplementasyonu

--- odev05/message_server.py
+++ odev05/message_server.py
@@ -59,3 +59,83 @@ class SendThread (threading.Thread):
             if not flag:
                 self.cSocket.send(userlist[self.nickname].get())
         self.lQueue.put("Exiting Send Thread of Process- " + str(self.name))
+
+#Mesaj alici thread implementasyonu
+class RecvThread (threading.Thread):
+    def __init__(self, name, cSocket, address, logQueue, threadQueue):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.cSocket = cSocket
+        self.address = address
+        self.lQueue = logQueue
+        self.tQueue = threadQueue
+        self.nick = ""
+    def run(self):
+        self.lQueue.put("Starting Recv Thread of Process- " + str(self.name))
+        self.flag = False
+        while not self.flag:
+            queue_message,response = self.parse(self.cSocket.recv(4096))
+            self.tQueue.put(response)
+            if queue_message != "":
+                self.lQueue.put(queue_message)
+        self.lQueue.put("Finishing Recv Thread of Process- " + str(self.name))
+
+    def parse(self,msg):
+        queue_message = ("")
+
+        if msg[0:3] == "USR":
+            if msg[3] == ' ':
+                nickname = msg [4:]
+                if nickname != "":
+                    try :
+                        userlist[nickname]
+                        response = "REJ " + nickname
+                    except KeyError:
+                        response = "HEL " + nickname
+                        self.nick = nickname
+                        user_queue = Queue.Queue(10)
+                        userlist[nickname]=user_queue
+                else:
+                    response = "ERR"
+            else:
+                response = "ERR"
+
+        elif msg[0:3] == "QUI":
+            queue_message = ("QUI",self.nick)
+            response = "BYE " + self.nick
+            self.flag = True
+            try:
+                del userlist[self.nick]
+            except:
+                pass
+
+        elif msg[0:3] == "LSQ":
+            users=""
+            for key in userlist:
+                users += key + ':'
+            response = "LSA " + users
+            queue_message = ("LSQ",self.nick)
+
+        elif msg[0:3] == "SAY":
+            for t in userlist:
+                if t != self.nick:
+                    userlist[t].put(self.nick+": "+ msg[4:])
+            queue_message = ("", self.nick, msg[4:])
+            response = "SOK"
+
+        elif msg[0:3] == "TIC":
+            response= "TOC"
+
+        elif msg[0:3] == "MSG":
+            to_nickname,message  = str.split(msg[4:],':',1)
+            try:
+                userlist[to_nickname].put(self.nick+": "+ message)
+                from_nickname = self.nick
+                queue_message = (to_nickname, from_nickname, message)
+                response = "MOK"
+            except KeyError:
+                response = "REJ " + to_nickname
+        else:
+            response = "ERR"
+            queue_message = ("ERR",self.nick)
+        return queue_message,response
