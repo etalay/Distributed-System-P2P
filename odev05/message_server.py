307674e2bfa41251ebbb373be2537f12ff3c35e7
etalay <talay.emre@gmail.com>
2015-11-17 19:22:26 +0200

    Log kayıtları için gerekli thread implentasyonunu

--- odev05/message_server.py
+++ odev05/message_server.py
@@ -9,3 +9,22 @@ import socket
 import threading
 import time
 import Queue
+
+#Log için gerekli Thread İmplementasyonunu gerceklestirdik.
+class LoggerThread (threading.Thread):
+    def __init__(self, logQueue, logFileName):
+        threading.Thread.__init__(self)
+        self.lQueue = logQueue
+        self.lFileName = logFileName
+    def log(self,message):
+        self.fid = open(self.lFileName,"a")
+        t = time.time()
+        self.fid.write("["+str(t)+"]: " + message)
+        self.fid.close()
+    def run(self):
+        self.log("Starting Logger Process\n")
+        while True:
+            if not self.lQueue.empty():
+                data = self.lQueue.get()
+                self.log(str(data)+"\n")
+        self.log("Finish Logger Process!\n")
