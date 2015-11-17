4b591d55f448a8be0a8dc1c861edbf2e5cf8176f
etalay <talay.emre@gmail.com>
2015-11-17 19:00:51 +0200

    Okuma thread'i için gerekli implementasyonu yaptık

--- odev05/message_client.py
+++ odev05/message_client.py
@@ -6,3 +6,18 @@ Created on Tue Nov 10 18:13:51 2015
 """
 import socket
 import threading
+
+#Okuma thread implmentasyonunu gerceklestirdik.
+class readThread (threading.Thread):
+    def __init__(self,clientSocket):
+        threading.Thread.__init__(self)
+        self.clientSocket = clientSocket
+        self.flag = False
+    def run(self):
+        print "Starting readThread "
+        while not self.flag:
+            data = self.clientSocket.recv(4096)
+            if data[0:3]=="BYE":
+                self.flag = True
+                self.clientSocket.close()
+            print data
