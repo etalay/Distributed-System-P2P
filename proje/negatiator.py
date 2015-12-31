9a578d87baf6ac35eca22fdb532e82c8df93ec1c
etalay <talay.emre@gmail.com>
2015-12-31 16:21:25 +0200

    Kullanılacak kütüphaneler import edildi.

---
+++ proje/negatiator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 21:13:51 2015
+@author: e-talay
+"""
+#!/usr/bin/env python
+
+import time
+import copy
+import threading
+import socket
+import Queue
