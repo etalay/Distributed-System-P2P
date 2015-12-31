df5e6eb7b1f123cf13e687dc17910c05aa4ff0a1
etalay <talay.emre@gmail.com>
2015-12-31 16:29:34 +0200

    kütüphane implementasyonu

---
+++ proje/peer.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 22:18:11 2015
+@author: e-talay
+"""
+#!/usr/bin/env python
+
+
+import Queue
+import time
+import sys
+import copy
+import threading
+import socket
